eb6b0ecae9ed11827d334dcecf1bf63c3420afa6
Davide Lasagna <lasagnadavide@gmail.com>
2020-06-30 11:01:48 +0100

    added perf script

---
+++ perf/perf_equations.jl
@@ -0,0 +1,39 @@
+using PoiseuilleFlow
+using FFTW
+using Printf
+using Flows
+
+P  = 91
+Lx = 6π
+α  = 2π / Lx
+Re = 1000
+Δt = 0.01;
+
+for LD in 20:200
+    # active number of waves
+    L = down_dealias_size(LD)
+
+    # create fields
+    ψ = PhysicalField(P, LD, Lx, (x, y)->0.06*cos(3*α*x)*(1-y^2)^2 + 
+                                         0.04*cos(5*α*x)*(1-y^2)^2 + 
+                                         0.02*cos(6*α*x)*(1-y^2)^2)
+    ψ̂ = SpectralField(P, L, LD)
+
+    # inverse transform
+    _ifft = InverseFFT!(ψ̂, FFTW.PATIENT, FFTW.NO_TIMELIMIT);
+    _fft  = ForwardFFT!(ψ, FFTW.PATIENT, FFTW.NO_TIMELIMIT);
+
+    # set initial condition
+    _fft(ψ̂, ψ);
+
+    # create equations
+    eq = StreamFunEquation(P, L, LD, Lx, Re, Δt; flags=FFTW.PATIENT, timelimit=FFTW.NO_TIMELIMIT)
+
+    # create flow operator
+    ϕ = flow(eq, eq, CNRK2(ψ̂), TimeStepConstant(Δt));
+
+    # march
+    t = minimum([@elapsed ϕ(ψ̂, (0, 10*Δt)) for i = 1:10])
+
+    @printf "%10.2f %04d\n" 10^9 * t / ((P+1) * (2LD+2) * log(2LD+2)) LD
+end
